7e5b6db8b4632418456d1c4c4e473b70821ff68b
domaingts <139693268+domaingts@users.noreply.github.com>
2024-10-10 12:50:07 +0000

    add an install script

---
+++ script/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+TEMPD=""
+
+add_systemd() {
+    curl -L -o /etc/systemd/system/ddns-go.service https://raw.githubusercontent.com/domaingts/ddns-go/refs/heads/master/script/ddns-go.service
+}
+
+make_configuration_folder() {
+  mkdir -p /etc/ddns-go
+}
+
+download() {
+    TEMPD="$(mktemp -d)"
+    local temp_file
+    temp_file="$(mktemp)"
+    if ! curl -sS -H "Accept: application/vnd.github.v3+json" -o "$temp_file" 'https://api.github.com/repos/ddns-go/realm/releases/latest'; then
+        "rm" "$temp_file"
+        echo 'error: Failed to get release list, please check your network.'
+    fi
+    version="$(sed 'y/,/\n/' "$temp_file" | grep 'tag_name' | awk -F '"' '{print $4}')"
+    "rm" "$temp_file"
+    local package="ddns-go-linux-amd64-v3.tar.gz"
+    curl -L "https://github.com/domaingts/ddns-go/releases/download/$version/$package" -o "$TEMPD/$package"
+    tar Cxzvf "$TEMPD" "$TEMPD/$package"
+    location="$TEMPD/ddns-go"
+    mv "$location" /usr/local/bin/
+    ddns-go -v | tee
+}
+
+main() {
+    add_systemd
+    make_configuration_folder
+    download
+}
+
+main
