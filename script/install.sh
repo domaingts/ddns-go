11ba2e305ba14a3d4b0dd437e9fb35aa10e6d34b
domaingts <139693268+domaingts@users.noreply.github.com>
2024-10-13 11:41:11 +0000

    update

--- script/install.sh
+++ script/install.sh
@@ -20,17 +20,24 @@ download() {
     temp_file="$(mktemp)"
     if ! curl -sS -H "Accept: application/vnd.github.v3+json" -o "$temp_file" 'https://api.github.com/repos/ddns-go/realm/releases/latest'; then
         "rm" "$temp_file"
+        "rm" -r "$TEMPD"
         echo 'error: Failed to get release list, please check your network.'
+        exit 1
     fi
     version="$(sed 'y/,/\n/' "$temp_file" | grep 'tag_name' | awk -F '"' '{print $4}')"
     "rm" "$temp_file"
     local package="ddns-go-linux-amd64-v3.tar.gz"
     echo 'https://github.com/domaingts/ddns-go/releases/download/$version/$package'
-    curl -L --retry 5 --retry-delay 10 --retry-max-time 60 "https://github.com/domaingts/ddns-go/releases/download/$version/$package" -o "$TEMPD/$package"
+    if ! curl -f -R -H ''Cache-Control: no-cache -o "$TEMPD/$package" "https://github.com/domaingts/ddns-go/releases/download/$version/$package"; then
+        "rm" -r "$TEMPD"
+        echo "removed: $TEMPD"
+        exit 1
+    fi
     tar Cxzvf "$TEMPD" "$TEMPD/$package"
     location="$TEMPD/ddns-go"
     mv "$location" /usr/local/bin/
     ddns-go -v | tee
+    "rm" -r "$TEMPD"
 }
 
 main() {
